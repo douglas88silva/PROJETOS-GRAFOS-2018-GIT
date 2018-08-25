33ab26919b43e4d22fe6f1ff8a28b4756f9518fa
Douglas da Silva <douglas88silva@hotmail.com>
2018-08-25 08:44:49 -0300

    Atividade 1  - Carregando arquivo txt e criando grafo com lista de adjacência.

--- Rascunho - Prototipos/Atividade 1/Proj_Grafos_ATV1/main.c
+++ Rascunho - Prototipos/Atividade 1/Proj_Grafos_ATV1/main.c
@@ -4,15 +4,16 @@
 #include "Arquivo.h"
 #include "Grafo.h"
 
-
-void Carrega_Arquivo()
-{
+///###################################################################
+///####################### CARREGANDO COM MENU #######################
+///###################################################################
+void Carrega_Arquivo(){
 
     char nome_arquivo[100];
     int opcao = -1;
 
     printf("Escolha uma das opcoes de Grado:");
-    printf("\n\n1 - Grafo ponderado\n");
+    printf("\n\n1 - Grafo ponderado");
     printf("\n0 - Grafo nao ponderado\n");
 
     while(opcao < 0 || opcao > 1)
@@ -21,10 +22,12 @@ void Carrega_Arquivo()
         setbuf(stdin, NULL);
     }
 
-    printf("Digite o nome do arquivo completo...\n");
+    printf("\n\nDigite o nome do arquivo completo...\n");
 
     scanf("%s", nome_arquivo);
     setbuf(stdin, NULL);
+
+
 
     printf("\n\nCarregando arquivo %s...\n", nome_arquivo);
 
@@ -50,14 +53,14 @@ void Carrega_Arquivo()
 
     if(grafo->eh_ponderado)
     {
-        int id1, id2, peso = 0;
+        int id1, id2, peso;
         int retorno;
         while (!feof(arquivo))
         {
-            retorno = fscanf(arquivo, "%d %d", &id1, &id2);
+            retorno = fscanf(arquivo, "%d %d %d", &id1, &id2,&peso);
             if (retorno != -1)
             {
-                printf("Inserindo aresta entre %d e %d\n", id1, id2);
+                printf("Inserindo aresta entre %d e %d = Peso %d\n", id1, id2, peso);
                 Popula_Grafo(grafo, id1, id2, peso);
             }
         }
@@ -65,14 +68,14 @@ void Carrega_Arquivo()
     }
     else
     {
-        int id1, id2,peso;
+        int id1, id2,peso = 0;
         int retorno;
         while (!feof(arquivo))
         {
-            retorno = fscanf(arquivo, "%d %d %d", &id1, &id2,&peso);
+            retorno = fscanf(arquivo, "%d %d", &id1, &id2);
             if (retorno != -1)
             {
-                printf("Inserindo aresta entre %d e %d\n", id1, id2);
+                printf("Inserindo aresta entre %d e %d = Peso %d\n", id1, id2, peso);
                 Popula_Grafo(grafo, id1, id2, peso);
             }
         }
@@ -93,10 +96,89 @@ void Carrega_Arquivo()
 
 
 
+///###################################################################
+///####################### CARREGANDO SEM MENU #######################
+///###################################################################
+
+Grafo *Carrega_Arquivo_Sem_Menu(int opcao, char *nome_arquivo)
+{
+
+    printf("\n\nCarregando arquivo %s...\n",nome_arquivo);
+
+    FILE *arquivo = Abre_Arquivo(nome_arquivo);
+
+    if (arquivo == NULL)
+     {
+         printf("Erro ao carregar o arquivo!");
+         return 0;
+     }
+
+    printf("Arquivo aberto...\n");
+
+    Grafo *novo_grafo = Cria_Novo_Grafo(opcao);
+
+    printf("Grafo criado...\n");
+
+    int total_vertice_arquivo = 0;
+
+    fscanf(arquivo, "%d", &total_vertice_arquivo);
+    printf("Total de vertices do arquivo: %d\n", total_vertice_arquivo);
+
+
+    if(novo_grafo->eh_ponderado)
+    {
+        int id1, id2, peso;
+        int retorno;
+        while (!feof(arquivo))
+        {
+            retorno = fscanf(arquivo, "%d %d %d", &id1, &id2,&peso);
+            if (retorno != -1)
+            {
+                printf("Inserindo aresta entre %d e %d = Peso %d\n", id1, id2, peso);
+                Popula_Grafo(novo_grafo, id1, id2, peso);
+            }
+        }
+    }
+    else
+    {
+        int id1, id2,peso = 0;
+        int retorno;
+        while (!feof(arquivo))
+        {
+            retorno = fscanf(arquivo, "%d %d", &id1, &id2);
+            if (retorno != -1)
+            {
+                printf("Inserindo aresta entre %d e %d = Peso %d\n", id1, id2, peso);
+                Popula_Grafo(novo_grafo, id1, id2, peso);
+            }
+        }
+    }
+
+
+
+    Fecha_Arquivo(arquivo);
+
+    printf("Grafo populado com sucesso!\n");
+    printf("Total de vertices cadastrados: %d\n", novo_grafo->Numero_Vertice);
+    printf("Total de arestas cadastradas: %d\n\n\n\n", novo_grafo->Numero_Aresta);
+
+
+    return novo_grafo;
+
+}
+
+
 int main()
 {
 
-        Carrega_Arquivo();
+        //Carrega_Arquivo();
+
+        char arq_grafo_ponderado[100] = "grafoPonderado.txt";
+        char arq_grafo[100] = "grafo.txt";
+
+        Grafo *grafo = Carrega_Arquivo_Sem_Menu(1,arq_grafo_ponderado);
+        Grafo *grafoPonderado = Carrega_Arquivo_Sem_Menu(0,arq_grafo);
+
 
 
 
