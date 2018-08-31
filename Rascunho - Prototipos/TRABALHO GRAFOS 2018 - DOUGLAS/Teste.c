9d1fda09a0880e8710a2dfd269ade76aa0ccce37
DOUGLAS DA SILVA <douglas.silva@ntendencia.com.br>
2018-08-31 14:59:17 -0300

    .

--- Rascunho - Prototipos/TRABALHO GRAFOS 2018 - DOUGLAS/Teste.c
+++ Rascunho - Prototipos/TRABALHO GRAFOS 2018 - DOUGLAS/Teste.c
@@ -1,8 +1,7 @@
 #include "Arquivo.h"
 #include "Grafo.h"
 
-void teste1()
-{
+void teste1(){
     FILE *arquivo = Abre_Arquivo("instance_1.txt");
 
     Imprime_Arquivo(arquivo);
@@ -10,8 +9,7 @@ void teste1()
     Fecha_Arquivo(arquivo);
 }
 
-void teste2()
-{
+void teste2(){
     FILE *arquivo = Cria_Arquivo("teste.txt");
 
     Escreve_Linha(arquivo, "abc");
@@ -20,8 +18,7 @@ void teste2()
     Fecha_Arquivo(arquivo);
 }
 
-void teste3()
-{
+void teste3(){
     Grafo *g = Cria_Novo_Grafo();
     Vertice *v1, *v2, *v3;
 
@@ -36,9 +33,7 @@ void teste3()
     Imprime_Todos_Vertices(g);
 }
 
-
-void teste4(char *nome_arquivo)
-{
+void teste4(char *nome_arquivo){
     FILE *arquivo = Abre_Arquivo(nome_arquivo);
 
     if (arquivo == NULL)
@@ -80,8 +75,8 @@ void teste4(char *nome_arquivo)
     Libera_Grafo(grafo);
 }
 
-void teste5(char *nome_arquivo)
-{
+void teste5(char *nome_arquivo){
+
     FILE *arquivo = Abre_Arquivo(nome_arquivo);
 
     if (arquivo == NULL)
@@ -106,6 +101,54 @@ void teste5(char *nome_arquivo)
     while (!feof(arquivo))
     {
         retorno = fscanf(arquivo, "%d %d", &id1, &id2);
+        if (retorno != -1)
+        {
+            printf("Inserindo aresta entre %d e %d\n", id1, id2);
+            Popula_Grafo(grafo, id1, id2, peso);
+        }
+    }
+
+    printf("Grafo populado com sucesso!\n");
+    printf("Total de vertices cadastrados: %d\n", grafo->Numero_Vertice);
+    printf("Total de arestas cadastradas: %d\n", grafo->Numero_Aresta);
+    printf("Imprimindo grafo lido...\n\n");
+
+    Imprime_Todos_Vertices(grafo);
+
+    Imprime_Todas_Arestas(grafo);
+
+
+    Libera_Grafo(grafo);
+
+}
+
+
+void teste6(char *nome_arquivo){
+
+    FILE *arquivo = Abre_Arquivo(nome_arquivo);
+
+    if (arquivo == NULL)
+        return;
+
+    printf("Arquivo aberto...\n");
+
+    Grafo *grafo = Cria_Novo_Grafo_Ponderado(1);
+
+    printf("Grafo criado\n...");
+
+    int total_vertice_arquivo = 0;
+
+    fscanf(arquivo, "%d", &total_vertice_arquivo);
+    printf("Total de vertices do arquivo: %d\n", total_vertice_arquivo);
+
+    printf("\nTrabalhando com hash...\n");
+
+
+    int id1, id2, peso;
+    int retorno;
+    while (!feof(arquivo))
+    {
+        retorno = fscanf(arquivo, "%d %d %d", &id1, &id2 , &peso);
         if (retorno != -1)
         {
             printf("Inserindo aresta entre %d e %d\n", id1, id2);
